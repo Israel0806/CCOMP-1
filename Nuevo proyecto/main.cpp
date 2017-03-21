7f2f6783bf7fef4bec6b75b67bf9efe33101a6aa
Israel0806 <israel.lopez@ucsp.edu.pe>
2017-03-21 10:38:51 -0500

    Add files via upload

--- Nuevo proyecto/main.cpp
+++ Nuevo proyecto/main.cpp
@@ -4,6 +4,26 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    float x,y;
+    float a,b,c,d;
+    cout<<"Ingrese el valor de x e y:\n";
+    cin>>x>>y;
+    a=x+y;
+    cout <<"La suma de "<<x<<"+"<<y<<" es: " << a << endl;
+
+    cout<<"Ingrese nuevos valores para x e y:\n";
+    cin>>x>>y;
+    b=x-y;
+    cout <<"La resta de "<<x<<"-"<<y<<" es: " << b << endl;
+
+    cout<<"Ingrese nuevos valores para x e y:\n";
+    cin>>x>>y;
+    c=x*y;
+    cout <<"La multiplicacion de "<<x<<"*"<<y<<" es: " << c << endl;
+
+    cout<<"Ingrese nuevos valores para x e y:\n";
+    cin>>x>>y;
+    d=x/y;
+    cout <<"La division de "<<x<<"/"<<y<<" es: " << d << endl;
     return 0;
 }
